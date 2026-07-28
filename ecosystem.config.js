e3daeb4f79d7aa62bcc4523e5b7a71c74f16bd9f
Abhinn Vyas <abhinnvyas2@gmail.com>
2026-07-28 11:06:19 +0530

    next build

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: "ui",
+            script: "cmd",
+            args: "/c npm run dev",
+            cwd: "D:\\Projects\\Sri-Lanka-UI",
+            interpreter: "none"
+        }
+    ]
+};
